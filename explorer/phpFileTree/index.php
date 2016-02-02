114f6fec3231fdc37ab6ec3ddabb6b36598bbdb1
Wolvenheart <michaux.michel@gmail.com>
2016-02-02 08:25:48 +0100

    begin les4

--- explorer/phpFileTree/index.php
+++ explorer/phpFileTree/index.php
@@ -12,10 +12,10 @@ $action = "javascript:window.open('[link]');";
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>PHP File Tree Demo</title>
+		<title>PHP File Tree</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<link href="styles/default/default.css" rel="stylesheet" type="text/css" media="screen" />
-		
+
 		<!-- Makes the file tree(s) expand/collapsae dynamically -->
 		<script src="php_file_tree.js" type="text/javascript"></script>
 	</head>
@@ -25,5 +25,5 @@ $action = "javascript:window.open('[link]');";
 		<script src="jquery.js" type="text/javascript"></script>
 		<script src="php_file_tree_jquery.js" type="text/javascript"></script>
 	</body>
-	
+
 </html>
