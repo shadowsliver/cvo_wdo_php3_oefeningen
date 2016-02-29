9ad5063bcdbc2a6ae97e9862fb0c229aeda8992a
Michel Michaux <webdesigner@407MAC02-15.local>
2016-02-29 13:44:15 +0100

    aze

--- blog_cvo/readme.md
+++
@@ -1,3 +0,0 @@
-#PHP3 Blog project
-
-Don't forget to adjust the database connection parameters in util/db.config.php to your situation!
--- blog_cvo/util/db.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbInfo = "mysql:host=localhost;dbname=simple_blog";
-$dbUser = 'homestead';
-$dbPassword = 'secret';
-
-$db = new PDO($dbInfo, $dbUser, $dbPassword);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
